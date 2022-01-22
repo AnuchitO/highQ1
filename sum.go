01774a0ace9d366ea5d1a096ca1160d0567f8750
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-22 10:39:32 +0700

    test variadic function

--- sum.go
+++ sum.go
@@ -1,5 +1,9 @@
 package sum
 
-func Sum(x, y int) int {
-	return x + y
+func Sum(xs ...int) int {
+	s := 0
+	for _, v := range xs {
+		s += v
+	}
+	return s
 }
--- sum_test.go
+++ sum_test.go
@@ -9,3 +9,13 @@ func TestSum(t *testing.T) {
 		t.Error("Expected 8, got", r)
 	}
 }
+
+func TestSumAll(t *testing.T) {
+	xs := []int{1, -2, 3}
+
+	r := Sum(xs...)
+
+	if r != 2 {
+		t.Error("Expected 6, got", r)
+	}
+}
