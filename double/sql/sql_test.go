2f5ff954a21eadf62d84c690cd3c343fd3e09862
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-29 10:57:57 +0700

    WORKSHOP : sql solution

--- double/sql/sql_test.go
+++ double/sql/sql_test.go
@@ -5,8 +5,28 @@ import (
 	"testing"
 )
 
+type stub struct {
+	query        string
+	lastInsertID int64
+	rowsAffected int64
+}
+
+func (m *stub) LastInsertId() (int64, error) {
+	return m.lastInsertID, nil
+}
+func (m *stub) RowsAffected() (int64, error) {
+	return m.rowsAffected, nil
+}
+
+func (m *stub) Exec(query string, args ...interface{}) (sql.Result, error) {
+	m.query = query
+	return m, nil
+}
+
 func TestExecQuery(t *testing.T) {
-	r, err := execQuery(&sql.DB{}, "SELECT 1", nil)
+	s := &stub{rowsAffected: 1}
+
+	r, err := execQuery(s, "SELECT 1", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
